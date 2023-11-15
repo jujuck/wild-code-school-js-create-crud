eb283bcc56b9710b0f8b41087e26f7e3e16c3d63
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-15 14:39:25 +0100

    Mise en place des champs de la table, récupération des infos

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const chalk = require("chalk");
-const { getTableName } = require("wild-crud-js/librairies/user");
+const { getTableName, getFields } = require("wild-crud-js/librairies/user");
 const { checkIfTableExist } = require("wild-crud-js/librairies/checks");
 
 
-const log = console.log();
-const error = console.error();
+const log = console.log;
+const error = console.error;
 
 (async () => {
+
   const table = await getTableName();
   if (await checkIfTableExist(table)) {
-    log("All good");
+    log(chalk.green(`Veuillez maintenant renseigner les champs de votre table.`));
+    log(`${chalk.green('Le champ')} id INT AUTO INCREMENT PRIMARY KEY NOT NULL est automatique`);
+    const fields = await getFields();
+
   } else {
     error(chalk.red(`La table -- ${table} -- semble déjà exister. Verifier puis recommencer !`))
   }
--- librairies/user.js
+++ librairies/user.js
@@ -1,13 +1,68 @@
 const { select, checkbox, confirm, input } = require("@inquirer/prompts");
-// import chalk from "chalk";
-
 const { getYamlInformationFromFile } = require("./files");
 
-const getTableName = () => {
-  const data = getYamlInformationFromFile("ressources/table.yml");
+/**
+ * Demande le nom de la table à l'utilisateur
+ * @returns Object { message : string}
+ */
+const getTableName = async () => {
+  const data = await getYamlInformationFromFile("ressources/table.yml");
   return input({ message: data.statement });
 };
 
+/**
+ * Convertit le champ YAML en object { name, value }
+ * @param [string] choices
+ * @returns Array[{name: string, value: string}]
+ */
+const getChoices = (choices) => {
+  return choices.map(ch => ({ name: ch, value: ch}))
+}
+
+/**
+ * Demande à l'utilisateur la définition des champs
+ * @returns Object {
+ *  fieldName: string,
+ *  fieldType: string,
+ *  long: string,
+ *  mandatory: string
+ * }
+ */
+const getField = async () => {
+  const { name, type, varchar, mandatory } = await getYamlInformationFromFile("ressources/fields.yml");
+  const fieldName = await input({ message: name.statement});
+  const fieldType = await select({ message: type.statement, choices: getChoices(type.answers)});
+  const long = fieldType === "VARCHAR" ?  await input({ message: varchar.statement}) : null;
+  const mandatoryField = await select({ message: mandatory.statement, choices: getChoices(mandatory.answers)});
+  return { fieldName, fieldType, long, mandatoryField}
+}
+
+/**
+ * Demande l'ensemble des champs à l'utililisateur
+ * @returns Array[{
+ *  fieldName: string,
+ *  fieldType: string,
+ *  long: string,
+ *  mandatory: string
+ * }]
+ */
+const getFields = async () => {
+  const fields = [];
+  let keepGoing = true
+  while (keepGoing) {
+    fields.push(await getField());
+    keepGoing = await select({
+      message: "Avez vous un autre champ à ajouter ?",
+      choices: [
+        { name: "Oui", value: true},
+        { name: "Non", value: false}
+      ]
+    })
+  }
+  return fields
+}
+
 module.exports = {
   getTableName,
+  getFields
 };
