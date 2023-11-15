5fffc184269887db1d0310b3a7650bb941be46f0
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-15 08:22:16 +0100

    Mise en place du message d'erreur si table existante

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const chalk = require("chalk");
 const { getTableName } = require("wild-crud-js/librairies/user");
 const { checkIfTableExist } = require("wild-crud-js/librairies/checks");
 
+
+const log = console.log();
+const error = console.error();
+
 (async () => {
   const table = await getTableName();
-  console.log(table);
   if (await checkIfTableExist(table)) {
-    console.log("All good");
+    log("All good");
+  } else {
+    error(chalk.red(`La table -- ${table} -- semble déjà exister. Verifier puis recommencer !`))
   }
 })();
