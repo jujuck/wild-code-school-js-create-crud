ede7324ec546826ddb9c459c5aacb35eb216fbf3
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-24 12:11:32 +0100

    Mise en commentaire des routes avec la validation

--- templates/routes.js
+++ templates/routes.js
@@ -4,12 +4,12 @@ const getTemplate = (table, validator, middleWareFolder) => {
   const middle = validator !== "none" ? ` ${table}Validation,` : ""
   return `
 const ${table}Controllers = require("./controllers/${table}Controllers");
-${validator !== "none" ? `const ${table}Validation = require("./${middleWareFolder}/${table}Validation");\n` : ""}
+${validator !== "none" ? `// const ${table}Validation = require("./${middleWareFolder}/${table}Validation");\n` : ""}
 router.get("/${table}s", ${table}Controllers.browse);
 router.get("/${table}s/:name", ${table}Controllers.read);
-router.post("/${table}s",${middle} ${table}Controllers.add);
-router.edit("/${table}s/:id",${middle} ${table}Controllers.edit);
-router.delete("/${table}s/:id", ${table}Controllers.destroy);
+// router.post("/${table}s",${middle} ${table}Controllers.add);
+// router.edit("/${table}s/:id",${middle} ${table}Controllers.edit);
+// router.delete("/${table}s/:id", ${table}Controllers.destroy);
 
 `;
 }
