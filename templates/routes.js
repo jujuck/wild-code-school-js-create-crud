60eb9c2dd943f094dfdefbda263ccaca9a9e93a0
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-24 12:17:16 +0100

    Correction de params dans la route get by id du router.js

--- templates/routes.js
+++ templates/routes.js
@@ -6,7 +6,7 @@ const getTemplate = (table, validator, middleWareFolder) => {
 const ${table}Controllers = require("./controllers/${table}Controllers");
 ${validator !== "none" ? `// const ${table}Validation = require("./${middleWareFolder}/${table}Validation");\n` : ""}
 router.get("/${table}s", ${table}Controllers.browse);
-router.get("/${table}s/:name", ${table}Controllers.read);
+router.get("/${table}s/:id", ${table}Controllers.read);
 // router.post("/${table}s",${middle} ${table}Controllers.add);
 // router.edit("/${table}s/:id",${middle} ${table}Controllers.edit);
 // router.delete("/${table}s/:id", ${table}Controllers.destroy);
